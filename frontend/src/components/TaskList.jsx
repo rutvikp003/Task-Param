269622a471bc5a1eda17a68350980a0eaf050534
Rutvik <rutvik.parmar.2103@gmail.com>
2026-01-20 00:40:03 +0530

    bug fixes

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,16 +1,25 @@
-import TaskCard from './TaskCard';
+import TaskCard from "./TaskCard";
 
-export default function TaskList({tasks, onEdit, onDelete}) {
+export default function TaskList({ tasks, onEdit, onDelete }) {
   if (!tasks || tasks.length === 0) {
-    return <div className="empty">No tasks yet - click <strong>Create tasks</strong> to add one</div>;
+    return (
+      <div className="empty">
+        No tasks yet - click <strong>Create tasks</strong> to add one
+      </div>
+    );
   }
   return (
     <>
-        <div className='task-grid'>
-          {tasks.map((task) => (
-            <TaskCard key={task.id} task={task} onEdit={() => onEdit(task)} onDelete = {()=> onDelete(task)}/>
-          ))}
-        </div>
+      <div className="task-grid">
+        {tasks.map((task) => (
+          <TaskCard
+            key={task._id}
+            task={task}
+            onEdit={() => onEdit(task)}
+            onDelete={() => onDelete(task)}
+          />
+        ))}
+      </div>
     </>
-   );
-};
+  );
+}
